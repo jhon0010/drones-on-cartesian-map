5974b3ca85a43b12813c40e1675df03cf75f16e2
jhon0010 <jhoning.soft@gmail.com>
2020-06-04 14:06:51 -0500

    Project structure DDD

---
+++ src/main/java/com/restaurant/RestaurantMain.java
@@ -0,0 +1,14 @@
+package com.restaurant;
+
+import org.slf4j.Logger;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class RestaurantMain {
+
+    private static final Logger LOGGER = getLogger(RestaurantMain.class);
+
+    public static void main(String[] args) {
+        LOGGER.info("Starting drone's restaurants delivery ..");
+    }
+}
